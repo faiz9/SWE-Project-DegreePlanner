b659f8473571506ee6fb95786a126bb732fddf8b
PianomanEric <erickristopherfalk@gmail.com>
2022-12-10 19:40:59 -0800

    Added a web scraping route to get all of SFSU's courses

--- Milestones/M4/BetaPrototype/server/routes/api/courses.js
+++ Milestones/M4/BetaPrototype/server/routes/api/courses.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../../config/database');
+const axios = require('axios');
+const { parse: parseHTML } = require('node-html-parser');
 
 router.get('/', (req, res) => {
-  db.query('SELECT * FROM demo').then(([results, fields]) => {
+  db.query('SELECT * FROM reference').then(([results, fields]) => {
     console.log(results);
     return res.json(results);
   }).catch((err) => {
@@ -17,7 +19,7 @@ router.get('/searchByRequirement', (req, res) => {
   console.log("Searching by requirement");
   const searchQuery = req.query.query;
   console.log(searchQuery);
-  db.query('SELECT * FROM demo WHERE subarea LIKE ?', [`%${searchQuery}%`]).then(([results, fields]) => {
+  db.query('SELECT * FROM reference WHERE subarea LIKE ?', [`%${searchQuery}%`]).then(([results, fields]) => {
     console.log(results);
     return res.json(results);
   }).catch((err) => {
@@ -30,7 +32,7 @@ router.get('/searchByRequirement', (req, res) => {
 router.get('/search', (req, res) => {
   const searchQuery = req.query.query.replaceAll(/( )/g, '');
   console.log(searchQuery);
-  db.query('SELECT * FROM demo WHERE codeID LIKE ?', [`%${searchQuery}%`]).then(([results, fields]) => {
+  db.query('SELECT * FROM reference WHERE codeID LIKE ?', [`%${searchQuery}%`]).then(([results, fields]) => {
     console.log(results);
     return res.json(results);
   }).catch((err) => {
@@ -40,9 +42,63 @@ router.get('/search', (req, res) => {
   });
 });
 
+// ^([a-zA-Z ]+\d+\w*) (.+) \(Units: (.+)\)$
+
+// Scrape all courses from SFSU's website
+// This might help for populating our database
+router.get('/scrape', (req, res) => {
+  axios.get('https://bulletin.sfsu.edu/courses').then((axiosRes) => {
+    const root = parseHTML(axiosRes.data);
+    const subjectAnchors = root.querySelectorAll('#atozindex li a');
+    const promises = [];
+    for (const anchor of subjectAnchors) {
+      const path = anchor.attributes.href
+      promises.push(new Promise((resolve, reject) => {
+        axios.get(`https://bulletin.sfsu.edu${path}`).then((axiosRes) => {
+          const root = parseHTML(axiosRes.data);
+          const courseDivs = root.querySelectorAll('.sc_sccoursedescs .courseblock');
+          const courses = [];
+          for (const div of courseDivs) {
+            const courseTitleElement = div.querySelector('.courseblocktitle strong');
+            const fullCourseTitle = courseTitleElement?.innerHTML.trim();
+            const results = fullCourseTitle?.match(/^([A-Z \u00A0]+\d+\w*)[ ]+(.+?) *\(Units: (.+)\)$/i);
+            if (results) {
+              const courseID = results[1].replaceAll(/[\u00A0 ]+/gi, '');
+              const title = results[2];
+              const units = results[3];
+              const courseDescriptionElement = div.querySelector('.courseblockdesc');
+              let description = courseDescriptionElement?.innerHTML || '';
+              for (const node of div.childNodes) {
+                if (node.nodeType == 3) {
+                  description += '\n' + node.innerText;
+                }
+              }
+              description = description.trim();
+              courses.push({courseID, title, units, description});
+            }
+          }
+          resolve(courses);
+        })
+      }));
+    }
+    Promise.all(promises).then((allCourses) => {
+      let count = 0;
+      const courseData = [];
+      for (const subjectCourses of allCourses) {
+        for (const course of subjectCourses) {
+          count += 1;
+          courseData.push(course);
+        }
+      }
+      console.log(`${count} courses found!`);
+      return res.json(courseData);
+    })
+  })
+});
+
 router.get('/:courseID', (req, res) => {
   const courseID = req.params.courseID;
-  db.query('SELECT * FROM demo WHERE codeID = ?', [courseID]).then(([results, fields]) => {
+  db.query('SELECT * FROM reference WHERE codeID = ?', [courseID]).then(([results, fields]) => {
     if (results.length == 1) {
       return res.json(results[0]);
     } else {
